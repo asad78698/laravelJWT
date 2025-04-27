190f42fa9eec5148cc2f4f45d27a777916b4b28c
Muhammad Asad <108513936+asad78698@users.noreply.github.com>
2025-04-27 17:45:31 +0500

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,8 +6,11 @@ use App\Models\User;
 use DB;
 use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash as FacadesHash;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWT;
 use Validator;
 
 class AuthController extends Controller
@@ -18,68 +21,61 @@ class AuthController extends Controller
         $user = new User();
 
         $user->name = $request->name;
-        dd($request->name);
 
         $user->email = $request->email;
-        $user->password = Hash::make($request->password);
+        $user->password = FacadesHash::make($request->password);
         $user->save();
-
-        $user->sendEmailVerificationNotification();
-
-        $token = JWTAuth::fromUser($user);
 
         return response()->json([
             'status' => 'success',
-            'data' => $user,
-            'token' => $token
+            'data' => $user
         ], 200);
 
     }
 
     public function login(Request $request)
     {
-
         $credentials = $request->only('email', 'password');
 
-        if (!$token = JWTAuth::attempt($credentials)) {
+        $validator = FacadesValidator::make($credentials, [
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required'
+        ]); 
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first(); 
+
+        if(!$user || !FacadesHash::check($request->password, $user->password)) {
             return response()->json([
                 'status' => 'error',
                 'message' => 'Invalid credentials'
             ], 401);
         }
 
+        $token = JWTAuth::attempt($credentials);
 
-        $user = auth()->user();
-
-        if (!$user->hasVerifiedEmail()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Email not verified'
-            ], 401);
-
-        }
+         
 
         return response()->json([
             'status' => 'success',
-            'token' => $token
-        ], 200);
+        ], 200)->cookie('token', $token, 60 * 24 * 7); // 7 days
+
     }
 
-    public function dashboard()
-    {
-
-
-        $asad = DB::table('users')->where('id', auth()->user()->id)->first();
-
-          
-
+    public function dashboard(){
+        $user = JWTAuth::user()->name;
         return response()->json([
             'status' => 'success',
-            'data' => $asad,
-            'value' => $asad->name
+            'message' => 'Welcome to the dashboard',
+            'user' => $user
         ], 200);
     }
-
     public function logout(Request $request)
     {
         $token = JWTAuth::getToken();
